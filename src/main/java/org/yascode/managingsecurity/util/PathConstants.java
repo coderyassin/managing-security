d162b77e34864c88621e1419c4d1720f878cd046
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2023-08-05 12:30:47 +0100

    spring security mul login

--- src/main/java/org/yascode/managingsecurity/util/PathConstants.java
+++ src/main/java/org/yascode/managingsecurity/util/PathConstants.java
@@ -5,7 +5,7 @@ public class PathConstants {
     public static class Student {
         public final static String STUDENT_PATH = "/student";
         public final static String STUDENT_HI = "/student-hi";
-        public final static String STUDENT_GET = "/student-get";;
+        public final static String STUDENT_GET = "/student-get";
     }
 
 }
