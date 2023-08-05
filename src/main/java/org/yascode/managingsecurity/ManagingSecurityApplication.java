f64606912eca67622b567e9a36e13625dc2aaf69
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2023-08-05 11:52:45 +0100

    create project

---
+++ src/main/java/org/yascode/managingsecurity/ManagingSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.yascode.managingsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagingSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagingSecurityApplication.class, args);
+	}
+
+}
